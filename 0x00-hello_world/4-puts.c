ee110f3e12cab77f4444b989117ac6afbf2624fe
Elijahjack63 <elijahjack526@yahoo.com>
2022-09-08 14:06:31 -0700

    Hello, printf and Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print in c
+ *
+ * Return: Always 0 (its successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
