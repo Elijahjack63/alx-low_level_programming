83b0a2541011aebadb49e7db7a7e2e91605d1487
Elijahjack63 <elijahjack526@yahoo.com>
2022-09-11 06:17:53 -0700

    program that prints the alphabet in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a' ; ch <= 'z' : ch++)
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 	if (ch != 'e' && ch != 'q')
 {
-	putcha(ch);
+	putchar(ch);
 }
 }
 putchar('\n');
