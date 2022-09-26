b787fadcfc7eb83e3c7af26f0c7b81543ad7a1fd
Elijahjack63 <elijahjack526@yahoo.com>
2022-09-26 16:43:40 -0700

    Write a function that copies memory area

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
  * char prototype()
  * unsigned int prototype()
  */
+int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_memset(char *s, char b, unsigned int n);
@@ -13,5 +14,7 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
+void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
 
 #endif /*MAIN_H*/
