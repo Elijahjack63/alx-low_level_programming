1ad4f5829b57a000195e037cef07574476260eda
Elijahjack63 <elijahjack526@yahoo.com>
2022-09-27 01:25:44 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,20 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
- * void prototype()
- * char prototype()
- * unsigned int prototype()
- */
 int _putchar(char c);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
-
-#endif /*MAIN_H*/
