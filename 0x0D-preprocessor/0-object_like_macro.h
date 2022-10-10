ed76f41adb8a5d569f9097505f526819fd0b3a62
Elijahjack63 <elijahjack526@yahoo.com>
2022-10-10 16:58:33 -0700

    Create a header file that defines a macro named SIZE as an abbreviation for the token 1024

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,5 @@
+#include "0-object_like_macro.h"
+#define TABLESIZE BUFSIZE
+#define BUFSIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio>
+
+/**
+ * This is header file
+ * its main.h file
+ *
+ * Return: Always success
+ */
+0-object_like_macro.h
+1-pi.h
+2-main.c
+3-function_like_macro.h
+4-sum.h
+
+#endif
