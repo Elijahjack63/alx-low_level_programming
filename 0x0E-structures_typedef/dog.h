fba857d7a6e42c011ac52915a957dde7bec59acb
Elijahjack63 <elijahjack526@yahoo.com>
2022-10-11 14:22:30 -0700

    conflict resolved

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,15 @@
-#include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include "stdio.h"
+#include "stdlib.h"
 
 /**
- * main - this program Define a new type struct dog
- * @char name : define the name of the dog
- * @float age: this state the age of the dog
+ * struct dog - dog struct
+ * @name: first member
+ * @owner: second member
+ * @age: third member
  *
- * Return: Always sucess
+ * Description: dog struct
  */
 struct dog
 {
@@ -13,3 +17,14 @@ struct dog
 	char *owner;
 	float age;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
