ff5c6c0930aab78592c427dca1bbf4627cb05a9b
Elijahjack63 <elijahjack526@yahoo.com>
2022-10-11 13:50:16 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this program Define a new type struct dog
+ * @char name : define the name of the dog
+ * @float age: this state the age of the dog
+ *
+ * Return: Always sucess
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+dog.h
+1-init_dog.c
+2-print_dog.c
+4-new_dog.c
+5-free_dog.c
+
+#endif
